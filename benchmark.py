ac26089d5a545df189b0bbfdb74f7a41e4555107
Johannes de Fine Licht <johannes@musicmedia.dk>
2016-06-16 15:35:16 +0100

    Multiprocessing benchmarking

--- benchmark.py
+++ benchmark.py
@@ -1,36 +1,46 @@
-import csv, time
+import csv, multiprocessing, sys, time
 import lqcd_batch, lqcd_single
 
 warmup = 1
 measurements = 5
-nRuns = [2**x for x in range(5, 17)]
-batchSizes = [2**x for x in range(1, 14)]
+nRuns = [16, 32]
+batchSizes = [2, 4]
+# nRuns = [2**x for x in range(5, 17)]
+# batchSizes = [2**x for x in range(1, 14)]
 highestRatio = 2**8
 
-results = []
-for n in nRuns:
-  print("-- {} runs".format(n))
-  if n < 2**11:
-    print("  Running sequential")
-    print("    Warmup runs...")
+def do_run(conf):
+  (n, b, reps) = conf
+  ret = []
+  print("Running {}/{}...".format(n, b))
+  if n == 1:
     for _ in range(warmup):
       lqcd_single.run_montecarlo(n)
-    for i in range(measurements):
+    for _ in range(reps):
       _, timeCompute, timeTotal = lqcd_single.run_montecarlo(n)
-      print("    Run {} / {}: {:.4f}/{:.4f} seconds".format(
-          i+1, measurements, timeCompute, timeTotal))
-      results.append((n, "1", timeCompute, timeTotal))
-  print("  Running in batches")
-  for b in [x for x in batchSizes if n % x == 0 and n / x <= highestRatio]:
-    print("    Batch size {}".format(b))
-    print("      Warmup runs...")
+      ret.append((n, b, timeCompute, timeTotal))
+  else:
     for _ in range(warmup):
-      lqcd_batch.run_montecarlo(b, int(n/b))
-    for i in range(measurements):
-      _, timeCompute, timeTotal = lqcd_batch.run_montecarlo(b, int(n/b))
-      print("      Run {} / {}: {:.4f}/{:.4f} seconds".format(
-          i+1, measurements, timeCompute, timeTotal))
-      results.append((n, b, timeCompute, timeTotal))
+      lqcd_batch.run_montecarlo(n, int(n/b))
+    for _ in range(reps):
+      _, timeCompute, timeTotal = lqcd_batch.run_montecarlo(n, int(n/b))
+      ret.append((n, b, timeCompute, timeTotal))
+  print("Finished {}/{}.".format(n, b))
+  return ret
+
+pool = multiprocessing.Pool(int(sys.argv[1]))
+
+jobs = []
+for n in nRuns:
+  if n < 2**11:
+    jobs.append((n, 1, measurements))
+  for b in [x for x in batchSizes if n % x == 0 and n / x <= highestRatio]:
+    jobs.append((n, b, measurements))
+
+res = pool.map(do_run, jobs)
+results = []
+for r in res:
+  results += r
 
 with open("benchmark.csv", "w") as outFile:
   writeCsv = csv.writer(outFile)
