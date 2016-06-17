a03322a637e35a13ae05eac627ecc24f4f8474f0
Johannes de Fine Licht <johannes@musicmedia.dk>
2016-06-17 18:18:20 +0100

    Added weak plot

--- plot.py
+++ plot.py
@@ -9,8 +9,13 @@ def filter_data(data):
     if int(b) not in accepted:
       del data[b]
 
+def set_style():
+  plt.style.use('ggplot')
+  plt.rcParams.update({'font.size': 16})
+
 def plot_strong(data, includeThermalize=False):
 
+  set_style()
   plt.style.use('ggplot')
   plt.rcParams.update({'font.size': 16})
   fig, ax = plt.subplots()
@@ -51,23 +56,95 @@ def plot_strong_single(data, nRuns):
   timeComputeErr = []
   timeTotal = []
   timeTotalErr = []
-  for b in data:
-    if nRuns not in data[b]["nRuns"]:
+  for b in sorted(data, key=int):
+    i = np.where(data[b]["nRuns"] == nRuns)[0]
+    if len(i) <= 0:
       continue
+    i = i[0]
     nBatches.append(int(b))
-    timeCompute.append(data[b]["timeCompute"])
-    timeComputeErr.append(data[b]["timeComputeErr"])
-    timeTotal.append(data[b]["timeTotal"])
-    timeTotalErr.append(data[b]["timeTotalErr"])
+    timeCompute.append(float(data[b]["timeCompute"][i]))
+    timeComputeErr.append(float(data[b]["timeComputeErr"][i]))
+    timeTotal.append(float(data[b]["timeTotal"][i]))
+    timeTotalErr.append(float(data[b]["timeTotalErr"][i]))
   nBatches = np.array(nBatches)
   timeCompute = np.array(timeCompute)
   timeComputeErr = np.array(timeComputeErr)
   timeTotal = np.array(timeTotal)
   timeTotalErr = np.array(timeTotalErr)
+  set_style()
   fig, ax = plt.subplots()
-  print(len(nBatches), len(timeCompute), len(timeComputeErr))
-  ax.errorbar(nBatches, timeCompute, timeComputeErr)
-  ax.errorbar(nBatches, timeTotal, timeTotalErr)
+  ax.set_xscale("log", basex=2)
+  ax.set_yscale("log", basey=2)
+  ax.set_xlabel("Batch size")
+  ax.set_ylabel("Time [s]")
+  ax.set_title("{} measurements".format(nRuns))
+  ax.errorbar(nBatches, timeCompute, timeComputeErr, linewidth=2,
+              marker="o", markersize=15, markerfacecolor="None",
+              label="Without thermalization",
+              color="red",
+              markeredgecolor="red",
+              markeredgewidth=2)
+  ax.errorbar(nBatches, timeTotal, timeTotalErr, linewidth=2,
+              marker="o", markersize=15, markerfacecolor="None",
+              label="With thermalization",
+              color="blue",
+              markeredgecolor="blue",
+              markeredgewidth=2)
+  ax.legend()
+  ax.set_xlim([0.9*ax.get_xlim()[0], ax.get_xlim()[1]*1.1])
+  ax.set_ylim([0.9*ax.get_ylim()[0], ax.get_ylim()[1]*1.1])
+  return fig
+
+def plot_weak(data):
+  nRuns = []
+  batchSize = []
+  timeCompute = []
+  timeComputeErr = []
+  timeTotal = []
+  timeTotalErr = []
+  n = 2**5
+  nBatch = min(map(int, list(data.keys())))
+  for b in sorted(data, key=int):
+    if int(b) != nBatch:
+      break
+    index = np.where(data[b]["nRuns"] == n)[0]
+    if len(index) <= 0:
+      break
+    index = index[0]
+    nRuns.append(n)
+    batchSize.append(int(b))
+    timeCompute.append(data[b]["timeCompute"][index]/n)
+    timeComputeErr.append(abs(timeCompute[-1])*data[b]["timeComputeErr"][index] /
+                   data[b]["timeCompute"][index])
+    timeTotal.append(data[b]["timeTotal"][index]/n)
+    timeTotalErr.append(abs(timeTotal[-1])*data[b]["timeTotalErr"][index] /
+                   data[b]["timeTotal"][index])
+    n *= 2
+    nBatch *= 2
+  batchSize = np.array(batchSize)
+  timeCompute = np.array(timeCompute)
+  timeComputeErr = np.array(timeComputeErr)
+  timeTotal = np.array(timeTotal)
+  timeTotalErr = np.array(timeTotalErr)
+  set_style()
+  fig, ax = plt.subplots()
+  ax.set_xscale("log", basex=2)
+  ax.set_yscale("log", basey=2)
+  ax.set_xlabel("Batch size")
+  ax.set_ylabel("Time per element [s/1]")
+  ax.errorbar(batchSize, timeCompute, timeComputeErr, linewidth=2,
+              marker="o", markersize=15, markerfacecolor="None",
+              label="Without thermalization",
+              color="red",
+              markeredgecolor="red",
+              markeredgewidth=2)
+  ax.errorbar(batchSize, timeTotal, timeTotalErr, linewidth=2,
+              marker="o", markersize=15, markerfacecolor="None",
+              label="With thermalization",
+              color="blue",
+              markeredgecolor="blue",
+              markeredgewidth=2)
+  ax.legend()
   return fig
 
 if __name__ == "__main__":
@@ -114,14 +191,15 @@ if __name__ == "__main__":
     data[b]["timeTotal"] = np.array(timeTotalMean)
     data[b]["timeTotalErr"] = np.array(timeTotalErr)
 
-  filter_data(data)
 
   if sys.argv[2] == "strong":
+    filter_data(data)
     fig = plot_strong(data, False)
   elif sys.argv[2] == "strong_thermalize":
+    filter_data(data)
     fig = plot_strong(data, True)
   elif sys.argv[2] == "strong_single":
-    fig = plot_strong_single(data, 2**10)
+    fig = plot_strong_single(data, 2**13)
   elif sys.argv[2] == "weak":
     fig = plot_weak(data)
   else:
